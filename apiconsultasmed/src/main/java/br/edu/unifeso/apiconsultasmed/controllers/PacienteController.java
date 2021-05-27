9c608a69904266833867da7fa38521bd288e0c19
Lucas Cruz <lucascs20182@gmail.com>
2021-05-27 20:33:20 -0300

    crie as rotas para o crud de paciente

--- apiconsultasmed/src/main/java/br/edu/unifeso/apiconsultasmed/controllers/PacienteController.java
+++ apiconsultasmed/src/main/java/br/edu/unifeso/apiconsultasmed/controllers/PacienteController.java
@@ -3,8 +3,11 @@ package br.edu.unifeso.apiconsultasmed.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +27,23 @@ public class PacienteController {
 		return pacienteService.listarTodos();
 	}
 	
+	@GetMapping("/{id}")
+	public PacienteModel listarUm(@PathVariable Integer id) {
+		return pacienteService.listarUm(id);
+	}
+	
 	@PostMapping("/cadastrar")
 	public void cadastrar(@RequestBody PacienteModel paciente) {
 		pacienteService.cadastrar(paciente);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletar(@PathVariable Integer id) {
+		pacienteService.deletar(id);
+	}
+	
+	@PutMapping("/{id}")
+	public void atualizarDados(@PathVariable Integer id, @RequestBody PacienteModel paciente) {
+		pacienteService.atualizarDados(id, paciente);
 	}
 }
